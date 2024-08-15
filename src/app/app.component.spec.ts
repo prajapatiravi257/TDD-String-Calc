52601365e6b214aee159bcbd543e4a509cb69d13
ravi.prajapati@comunus.in <ravi.prajapati@comunus.in>
2024-08-15 11:29:23 +0530

    inital setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tdd-string-calc' title`, () => {
+  it(`should have the 'String Calculator' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tdd-string-calc');
+    expect(app.title).toEqual('String Calculator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tdd-string-calc');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, String Calculator'
+    );
   });
 });
