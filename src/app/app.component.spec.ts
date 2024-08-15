6a45f2d2684762a3c9edb660a2bb7b3cee76dcc5
ravi.prajapati@comunus.in <ravi.prajapati@comunus.in>
2024-08-15 14:45:29 +0530

    updated cases and app comp code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,23 +6,23 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let debugElement: DebugElement;
+  let component: AppComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
     fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     debugElement = fixture.debugElement;
   });
 
   it('should create the app', () => {
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have the 'String Calculator' title`, () => {
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('String Calculator');
+    expect(component.title).toEqual('String Calculator');
   });
 
   it('should render title', () => {
@@ -42,54 +42,84 @@ describe('AppComponent', () => {
     expect(inputLabel.nativeElement).toBeTruthy();
   });
 
-  it('should accepts any number', () => {
-    const stringInput = debugElement.query(By.css('input[id=stringInput]'));
-    stringInput.nativeElement.value = 'j5BC*&^';
-    stringInput.nativeElement.dispatchEvent(new Event('input'));
+  // it('should accepts delimeters and non negative numbers', () => {
+  //   const stringInput = debugElement.query(By.css('input[id=stringInput]'));
+  //   stringInput.nativeElement.value = '1,\n2;//4\\2;-1,-2';
+  //   stringInput.nativeElement.dispatchEvent(new Event('input'));
 
-    fixture.detectChanges();
+  //   fixture.detectChanges();
 
-    fixture.whenStable().then(() => {
-      fixture.detectChanges();
-      console.log('sendInput : ', stringInput.nativeElement.value);
-      expect(stringInput.nativeElement.value).toMatch('.*[0-9].*');
-    });
+  //   fixture.whenStable().then(() => {
+  //     fixture.detectChanges();
+
+  //     let cleanedString: string = stringInput.nativeElement.value.replace(
+  //       /[^0-9-]/g,
+  //       ' '
+  //     );
+
+  //     console.log('cleanedString', cleanedString);
+
+  //     let allNumbersString = cleanedString.trim().split(/\s+/);
+  //     console.log('allNumbersString', cleanedString);
+
+  //     let allNumbers: number[] = allNumbersString
+  //       .map(Number)
+  //       .filter((num) => !isNaN(num));
+
+  //     console.log('filtered numbers : ', allNumbers);
+
+  //     let negativeNumbers = allNumbers.filter((num) => num < 0);
+  //     console.log('negative numbers : ', negativeNumbers);
+
+  //     expect(negativeNumbers)
+  //       .withContext(
+  //         `Negative numbers not allowed ${negativeNumbers.toString()}`
+  //       )
+  //       .toBe([]);
+  //   });
+  // });
+
+  it('should return 0 for an empty string', () => {
+    const result = component.add('');
+    expect(result).toBe(0);
   });
 
-  it('should accepts delimeters and non negative numbers', () => {
-    const stringInput = debugElement.query(By.css('input[id=stringInput]'));
-    stringInput.nativeElement.value = '1,\n2;//4\\2;-1,-2';
-    stringInput.nativeElement.dispatchEvent(new Event('input'));
+  it('should return the number for a single number string', () => {
+    const result = component.add('1');
+    expect(result).toBe(1);
+  });
 
-    fixture.detectChanges();
+  it('should return the sum of two numbers separated by a comma', () => {
+    const result = component.add('1,5');
+    expect(result).toBe(6);
+  });
 
-    fixture.whenStable().then(() => {
-      fixture.detectChanges();
+  it('should accept newline delimiter', () => {
+    const result = component.add('1\n2,3');
+    expect(result).toBe(6);
+  });
 
-      let cleanedString: string = stringInput.nativeElement.value.replace(
-        /[^0-9-]/g,
-        ' '
-      );
+  it('should accept any delimiter //;\n1;2', () => {
+    const result = component.add('//;\n1;2');
+    expect(result).toBe(3);
+  });
 
-      console.log('cleanedString', cleanedString);
+  it('should accept any delimiter //&\n1&3', () => {
+    const result = component.add('//&\n1&3');
+    expect(result).toBe(4);
+  });
 
-      let allNumbersString = cleanedString.trim().split(/\s+/);
-      console.log('allNumbersString', cleanedString);
+  it('should accept any delimiter //$\n1$4', () => {
+    const result = component.add('//$\n1$4');
+    expect(result).toBe(5);
+  });
 
-      let allNumbers: number[] = allNumbersString
-        .map(Number)
-        .filter((num) => !isNaN(num));
-
-      console.log('filtered numbers : ', allNumbers);
-
-      let negativeNumbers = allNumbers.filter((num) => num < 0);
-      console.log('negative numbers : ', negativeNumbers);
-
-      expect(negativeNumbers)
-        .withContext(
-          `Negative numbers not allowed ${negativeNumbers.toString()}`
-        )
-        .toBe([]);
-    });
+  it('should not accept negative numbers', () => {
+    try {
+      component.add('//;\n1;-2');
+    } catch (error: any) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toContain('Negative numbers not allowed');
+    }
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,38 @@ export class AppComponent {
   title = 'String Calculator';
   stringInput: string = '';
 
-  add(numbers: string) {}
+  add(numbers: string): number {
+    console.log('input: ', numbers);
+    if (!numbers.match('.*[0-9].*')) {
+      return 0;
+    }
+
+    let cleanedString: string = numbers.replace(/[^0-9-]/g, ' ');
+
+    console.log('cleanedString', cleanedString);
+
+    let allNumbersString = cleanedString.trim().split(/\s+/);
+    console.log('allNumbersString', cleanedString);
+
+    let allNumbers: number[] = allNumbersString
+      .map(Number)
+      .filter((num) => !isNaN(num));
+
+    console.log('filtered numbers : ', allNumbers);
+
+    let negativeNumbers = allNumbers.filter((num) => num < 0);
+    console.log('negative numbers : ', negativeNumbers);
+
+    if (negativeNumbers.length > 0) {
+      throw new Error(
+        `Negative numbers not allowed ${negativeNumbers.toString()}`
+      );
+    }
+
+    let total = allNumbers.reduce((x, y) => x + y);
+
+    console.log('Total : ', total);
+
+    return total;
+  }
 }
